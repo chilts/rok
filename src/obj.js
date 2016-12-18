eb0638b6d0b2b29577a32e91680cd777a0fbceb5
Andrew Chilton <andychilton@gmail.com>
2016-12-19 12:59:59 +1300

    Fix typo RobObject -> RokObject

--- src/obj.js
+++ src/obj.js
@@ -18,26 +18,26 @@ function RokObject() {
 }
 inherits(RokObject, Rok)
 
-RobObject.prototype.props = function props() {
+RokObject.prototype.props = function props() {
   return [ 'obj' ]
 }
 
-RobObject.prototype._resetProps = function _resetProps() {
+RokObject.prototype._resetProps = function _resetProps() {
   // called by Rok.reset()
   this.obj = null
 }
 
-RobObject.prototype.get = function get() {
+RokObject.prototype.get = function get() {
   return this.obj
 }
 
-RobObject.prototype.set = function set(obj) {
+RokObject.prototype.set = function set(obj) {
   this.obj = obj
   this.notify()
 }
 
 // --------------------------------------------------------------------------------------------------------------------
 
-module.exports = RobObject
+module.exports = RokObject
 
 // --------------------------------------------------------------------------------------------------------------------
