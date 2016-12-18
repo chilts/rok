d18747628a2bef2381017e030bece4e51028235e
Andrew Chilton <andychilton@gmail.com>
2016-12-19 12:26:17 +1300

    Add RokObject

---
+++ src/obj.js
@@ -0,0 +1,43 @@
+// --------------------------------------------------------------------------------------------------------------------
+
+"use strict"
+
+// npm
+var inherits = require('inherits')
+
+// local
+var Rok = require('./rok.js')
+
+// --------------------------------------------------------------------------------------------------------------------
+
+Function RokObject() {
+  Rok.call(this)
+
+  // simply call reset to set up our properties
+  this.reset()
+}
+inherits(RokObject, Rok)
+
+RobObject.prototype.props = function props() {
+  return [ 'obj' ]
+}
+
+RobObject.prototype._resetProps = function _resetProps() {
+  // called by Rok.reset()
+  this.obj = null
+}
+
+RobObject.prototype.get = function get() {
+  return this.obj
+}
+
+RobObject.prototype.set = function set(obj) {
+  this.obj = obj
+  this.notify()
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+module.exports = RobObject
+
+// --------------------------------------------------------------------------------------------------------------------
