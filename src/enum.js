2d0c297b0fd8bca061cd7845ac480a8f20832982
Andrew Chilton <andychilton@gmail.com>
2016-12-22 10:16:06 +1300

    Use name and enums, instead of val and values

--- src/enum.js
+++ src/enum.js
@@ -25,8 +25,8 @@ function RokEnum(name, title, opts) {
   // start off with empty 'valid' and 'selected'
   this.valid = {}
   this.selected = {}
-  // also store 'valid' values in an array so we can keep order
-  this.values = []
+  // also store 'valid' names in an array so we can keep order
+  this.enums = []
 }
 inherits(RokEnum, Rok)
 
@@ -35,7 +35,7 @@ RokEnum.prototype.props = function props() {
 }
 
 RokEnum.prototype.objects = function props() {
-  return [ 'meta', 'valid', 'values', 'selected', ]
+  return [ 'meta', 'valid', 'enums', 'selected', ]
 }
 
 // Called by Rok.reset().
@@ -45,172 +45,175 @@ RokEnum.prototype._resetProps = function _resetProps() {
   this.selected = {}
 }
 
-  RokEnum.prototype.getName = function getName() {
-    return this.name
+RokEnum.prototype.getName = function getName() {
+  return this.name
+}
+
+RokEnum.prototype.getTitle = function getTitle() {
+  return this.title
+}
+
+RokEnum.prototype.setTitle = function setTitle(title) {
+  this.title = title
+  this.notify()
+}
+
+RokEnum.prototype.setMeta = function setMeta(meta) {
+  this.meta = meta
+  this.notify()
+}
+
+RokEnum.prototype.getMeta = function getMeta() {
+  return this.meta
+}
+
+RokEnum.prototype.getMax = function getMax() {
+  return this.max || Object.keys(this.valid).length
+}
+
+RokEnum.prototype.add = function add(name, value) {
+  if ( value ) {
+    this.valid[name] = value
+  }
+  else {
+    this.valid[name] = true
+  }
+  this.enums.push({ name : name, value : value })
+  this.notify()
+}
+
+RokEnum.prototype.del = function del(name) {
+  delete this.valid[name]
+  // ToDo: also remove from `this.enums`
+  this.notify()
+}
+
+// returns the names of all enums that are valid (not the enums)
+RokEnum.prototype.getAllValidNames = function getAllValidNames() {
+  return Object.keys(this.valid)
+}
+
+// returns this list of all enum objects (which all have (name, value))
+RokEnum.prototype.getAllEnums = function getAllEnums() {
+  return this.enums
+}
+
+// returns the names of all enums selected (not the enums)
+RokEnum.prototype.getAllSelected = function getAllSelected() {
+  return Object.keys(this.selected)
+}
+
+// returns an object of { name : true, ... } names
+RokEnum.prototype.getSelectedAsObj = function getSelectedAsObj() {
+  var selected = {}
+  Object.keys(this.selected).forEach(function(name) {
+    selected[name] = true
+  })
+  return selected
+}
+
+RokEnum.prototype.isAllowed = function isAllowed(name) {
+  return name in this.valid
+}
+
+// returns the { name : '...', value : ... } object
+RokEnum.prototype.get = function get(name) {
+  return this.valid[name]
+}
+
+RokEnum.prototype.isSet = function isSet(name) {
+  return !!this.selected[name]
+}
+
+// returns the (meta) value stored with the enum
+RokEnum.prototype.getSelected = function getSelected(name) {
+  if ( !this.isAllowed(name) ) {
+    throw new Error("RokEnum: trying to get a selected enum '" + name + "' for enum '" + this.name + "' which isn't valid")
   }
 
-  RokEnum.prototype.getTitle = function getTitle() {
-    return this.title
+  // if selected, then return the enum value
+  if ( this.selected[name] ) {
+    return this.valid[name]
   }
 
-  RokEnum.prototype.setTitle = function setTitle(title) {
-    this.title = title
-    this.notify()
+  // not selected, just return null
+  return null
+}
+
+RokEnum.prototype.countValid = function countValid() {
+  return Object.keys(this.valid).length
+}
+
+RokEnum.prototype.countSelected = function countSelected() {
+  return Object.keys(this.selected).length
+}
+
+RokEnum.prototype.set = function set(name) {
+  // check that this val is valid
+  if ( !this.isAllowed(name) ) {
+    throw new Error("RokEnum: name '" + name +  "' for enum '" + this.name + "' is not a valid enumeration")
   }
 
-  RokEnum.prototype.setMeta = function setMeta(meta) {
-    this.meta = meta
-    this.notify()
+  // if this name is already set, do nothing and return
+  if ( name in this.selected ) {
+    return
   }
 
-  RokEnum.prototype.getMeta = function getMeta() {
-    return this.meta
+  // now check if we are trying to set too many
+  if ( this.countSelected() + 1 > this.getMax() ) {
+    throw new Error("RokEnum: trying to set too many selections (allowed: " + this.getMax() + ", currently: " + this.countSelected() + ")")
   }
 
-  RokEnum.prototype.getMax = function getMax() {
-    return this.max || Object.keys(this.valid).length
+  // all good
+  this.selected[name] = true
+  this.notify()
+}
+
+RokEnum.prototype.unset = function unset(name) {
+  // check that this name is valid
+  if ( !this.isAllowed(name) ) {
+    throw new Error("RokEnum: trying to unset name '" + name +  "' when it is not a valid enumeration")
   }
 
-  RokEnum.prototype.add = function add(val, meta) {
-    if ( meta ) {
-      this.valid[val] = meta
-    }
-    else {
-      this.valid[val] = true
-    }
-    this.values.push({ val : val, meta : meta })
-    this.notify()
+  // doesn't matter if this is already unset, just delete it anyway
+  delete this.selected[name]
+  this.notify()
+}
+
+RokEnum.prototype.toggle = function toggle(name) {
+  // check that this name is valid
+  if ( !this.isAllowed(name) ) {
+    throw new Error("RokEnum: trying to toggle name '" + name +  "' for enum '" + this.name + "' when it is not a valid enumeration")
   }
 
-  RokEnum.prototype.del = function del(val) {
-    delete this.valid[val]
-    this.notify()
+  // just call unset() or set() (which both emit 'update')
+  if ( name in this.selected ) {
+    this.unset(name)
+  }
+  else {
+    this.set(name)
+  }
+}
+
+RokEnum.prototype.setTo = function setTo(name, to) {
+  // check that this name is valid
+  if ( !this.isAllowed(name) ) {
+    throw new Error("RokEnum: trying to setTo name '" + name +  "' for enum '" + this.name + "' when it is not a valid enumeration")
   }
 
-  // returns the names of all enums that are valid (not the values)
-  RokEnum.prototype.getAllValid = function getAllValid() {
-    return Object.keys(this.valid)
+  // just call unset() or set() (which both emit 'update')
+  if ( to ) {
+    this.set(name)
   }
-
-  RokEnum.prototype.getAllValues = function getAllValues() {
-    return this.values
+  else {
+    this.unset(name)
   }
+}
 
-  // returns the names of all enums selected (not the values)
-  RokEnum.prototype.getAllSelected = function getAllSelected() {
-    return Object.keys(this.selected)
-  }
-
-  // returns an object of { name : true, ... } values
-  RokEnum.prototype.getSelectedAsObj = function getSelectedAsObj() {
-    var selected = {}
-    Object.keys(this.selected).forEach(function(name) {
-      selected[name] = true
-    })
-    return selected
-  }
-
-  RokEnum.prototype.isAllowed = function isAllowed(val) {
-    return val in this.valid
-  }
-
-  RokEnum.prototype.getValid = function getValid(val) {
-    return this.valid[val]
-  }
-
-  RokEnum.prototype.isSet = function isSet(val) {
-    return !!this.selected[val]
-  }
-
-  // returns the (meta) value stored with the enum
-  RokEnum.prototype.getSelected = function getSelected(val) {
-    if ( !this.isAllowed(val) ) {
-      throw new Error("RokEnum: trying to get a selected value '" + val + "' which isn't valid")
-    }
-
-    // if selected, then return the enum value
-    if ( this.selected[val] ) {
-      return this.valid[val]
-    }
-
-    // not selected, just return null
-    return null
-  }
-
-  RokEnum.prototype.countValid = function countValid() {
-    return Object.keys(this.valid).length
-  }
-
-  RokEnum.prototype.countSelected = function countSelected() {
-    return Object.keys(this.selected).length
-  }
-
-  RokEnum.prototype.set = function set(val) {
-    // check that this val is valid
-    if ( !this.isAllowed(val) ) {
-      throw new Error("RokEnum: value '" + val +  "' for enum '" + this.name + "' is not a valid enumeration")
-    }
-
-    // if this value is already set, do nothing and return
-    if ( val in this.selected ) {
-      return
-    }
-
-    // now check if we are trying to set too many
-    if ( this.countSelected() + 1 > this.getMax() ) {
-      throw new Error("RokEnum: trying to set too many selections (allowed: " + this.getMax() + ", currently: " + this.countSelected() + ")")
-    }
-
-    // all good
-    this.selected[val] = true
-    this.notify()
-  }
-
-  RokEnum.prototype.unset = function unset(val) {
-    // check that this val is valid
-    if ( !this.isAllowed(val) ) {
-      throw new Error("RokEnum: trying to unset value '" + val +  "' when it is not a valid enumeration")
-    }
-
-    // doesn't matter if this is already unset, just delete it anyway
-    delete this.selected[val]
-    this.notify()
-  }
-
-  RokEnum.prototype.toggle = function toggle(val) {
-    // check that this val is valid
-    if ( !this.isAllowed(val) ) {
-      throw new Error("RokEnum: trying to toggle value '" + val +  "' when it is not a valid enumeration")
-    }
-
-    // just call unset() or set() (which both emit 'update')
-    if ( val in this.selected ) {
-      this.unset(val)
-    }
-    else {
-      this.set(val)
-    }
-  }
-
-  RokEnum.prototype.setTo = function setTo(val, to) {
-    // check that this val is valid
-    if ( !this.isAllowed(val) ) {
-      throw new Error("RokEnum: trying to setTo value '" + val +  "' when it is not a valid enumeration")
-    }
-
-    // just call unset() or set() (which both emit 'update')
-    if ( to ) {
-      this.set(val)
-    }
-    else {
-      this.unset(val)
-    }
-  }
-
-  RokEnum.prototype.clearSelected = function clearSelected() {
-    this.selected = {}
-    this.notify()
-  }
+RokEnum.prototype.clearSelected = function clearSelected() {
+  this.selected = {}
+  this.notify()
+}
 
 // --------------------------------------------------------------------------------------------------------------------
 
